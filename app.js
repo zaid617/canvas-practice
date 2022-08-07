90a90ba27300f411b5ed54f98821bae47d6f890a
Zaid_ZiD <zid6177@gmail.com>
2022-08-07 14:13:52 +0500

    changes in app.js

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
+let canvas = document.getElementById("myCanvas");
+canvas.width = window.innerWidth - 250;
+canvas.height = window.innerHeight - 250;
 
+let ctx = document.getElementById("myCanvas").getContext("2d");
+ctx.fillStyle = "rgba(0,0,0)"
 
-    let ctx = document.getElementById("myCanvas").getContext("2d");
-    ctx.fillStyle = "rgba(0,0,0)"
+let isMouseDown = false;
 
-    let isMouseDown = false;
+document.querySelector("#myCanvas").addEventListener("mousedown", (event) => {
+    isMouseDown = true;
+});
 
-    document.querySelector("#myCanvas").addEventListener("mousedown", (event) => {
-        isMouseDown = true;
-    });
-    document.querySelector("#myCanvas").addEventListener("mouseup", (event) => {
-        isMouseDown = false;
-    });
+document.querySelector("#myCanvas").addEventListener("mouseup", (event) => {
+    isMouseDown = false;
+    ctx.beginPath()
+});
 
-    document.querySelector("#myCanvas").addEventListener("mousemove", (event) => {
+document.querySelector("#myCanvas").addEventListener("mousemove", (event) => {
 
-        if (isMouseDown) {
-            console.log("move: ", event);
-            console.log("h: ", event.offsetX);
-            console.log("w: ", event.offsetY);
+    if (isMouseDown) {
+        console.log("move: ", event);
+        console.log("h: ", event.offsetX);
+        console.log("w: ", event.offsetY);
 
-            ctx.fillRect(
-                event.offsetX,
-                event.offsetY,
-                5, 5
-            );
+        ctx.lineTo(
+            event.clientX - canvas.offsetLeft,
+            event.clientY - canvas.offsetTop
+          );
+          
+          ctx.lineCap = "round";
+          ctx.lineJoin = "round";
+          ctx.lineWidth = 10;
+          ctx.stroke();
+          return  
 
         }
 
     });
+
